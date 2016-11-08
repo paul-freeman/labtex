d133f1c686e6353fb66486a26422551df0860ae4
Paul Freeman <pfre484@aucklanduni.ac.nz>
2016-11-08 17:04:14 +1300

    refactoring to ease usage

--- src/nz/ac/auckland/labtex/Main.java
+++ src/nz/ac/auckland/labtex/Main.java
@@ -2,15 +2,21 @@ package nz.ac.auckland.labtex;
 
 import java.io.FileReader;
 
-import nz.ac.auckland.labtex.generated.parser;
-import nz.ac.auckland.labtex.generated.Scanner;
+import nz.ac.auckland.labtex.generated.LabParser;
+import nz.ac.auckland.labtex.generated.LabLexer;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        LabLexer lexer;
+		LabParser parser;
 		try {
-            Scanner scanner = new Scanner(new FileReader(args[0]));
-			parser p = new parser(scanner);
-			Object result = p.parse().value;
+			lexer = new LabLexer(new FileReader(args[0]));
+
+            /* deprecated in favor of new ComplexSymbolFactory */
+            /* but seems like premature deprecation to me */
+			parser = new LabParser(lexer);
+
+			Object result = parser.parse().value;
 		}
 		catch (Exception e) {
 			System.out.println("There was an error: " + e.getMessage());
